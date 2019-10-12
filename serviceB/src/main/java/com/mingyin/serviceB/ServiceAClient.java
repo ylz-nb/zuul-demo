3b41e6a0a0198269cb44ec81263d00e11883f8ce
yanglianze <1654003026@qq.com>
2019-10-12 20:34:15 +0800

    a

---
+++ serviceB/src/main/java/com/mingyin/serviceB/ServiceAClient.java
@@ -0,0 +1,10 @@
+package com.mingyin.serviceB;
+
+import com.mingyin.api.ServiceAInterface;
+import org.springframework.cloud.netflix.feign.FeignClient;
+
+@FeignClient("ServiceA")
+public interface ServiceAClient extends ServiceAInterface {
+
+    String sayBye（String id, String name);
+}
